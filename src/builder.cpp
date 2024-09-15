ad2368bde4232845a837f59efa963ce046321fdb
gie404 <ketchup404@gmail.com>
2024-09-15 18:27:32 +0300

    bag of bugs...

--- src/builder.cpp
+++ src/builder.cpp
@@ -1,5 +1,6 @@
 #include "iostream"
 #include <algorithm>
+#include <iomanip>
 #include "builder.h"
 #include "point.h"
 #include "line.h"
@@ -7,6 +8,36 @@
 #include "intersect.h"
 
 using namespace std;
+
+void printSubTree(const Curve *node, int indent);
+
+void printSubTree(const Point *node, int indent)
+{
+    if (node != nullptr)
+    {
+        cout << string(indent, ' ') << *node << endl;
+        printSubTree(node->par1, indent + 4);
+        printSubTree(node->par2, indent + 4);
+    }
+}
+void printSubTree(const Curve *node, int indent)
+{
+    if (node != nullptr)
+    {
+        cout << string(indent, ' ');
+        if (node->type == 0)
+        {
+            cout << *static_cast<const Line *>(node);
+        }
+        if (node->type == 1)
+        {
+            cout << *static_cast<const Circle *>(node);
+        }
+        cout << endl;
+        printSubTree(node->par1, indent + 4);
+        printSubTree(node->par2, indent + 4);
+    }
+}
 
 Builder::Builder()
 {
@@ -17,16 +48,30 @@ Builder::Builder()
 void Builder::processLoop(const Point &p1, const Point &p2)
 {
     cout << "Loop " << p1 << " " << p2 << endl;
+    printSubTree(&p1, 0);
+    printSubTree(&p2, 0);
 }
 
 void Builder::processLoop(const Line &l1, const Line &l2)
 {
     cout << "Loop " << l1 << " " << l2 << endl;
+    cout << "basep\n";
+    for(auto &p: basePoints){
+        cout << p << endl;
+    }
+    cout << "basel\n";
+    for(auto &p: baseLines){
+        cout << p << " " << *p.par1 << " " << *p.par2 << endl;
+    }
+    printSubTree(&l1, 0);
+    printSubTree(&l2, 0);
 }
 
 void Builder::processLoop(const Circle &c1, const Circle &c2)
 {
     cout << "Loop " << c1 << " " << c2 << endl;
+    printSubTree(&c1, 0);
+    printSubTree(&c2, 0);
 }
 
 bool Builder::findLoop(const Point &p, const std::vector<Point> &points)
@@ -81,45 +126,30 @@ bool Builder::isNewCircle(const Circle &c)
            findLoop(c, stageCircles);
 }
 
-void Builder::addPointToBase(const Point &p)
+void Builder::addPointToBase(const Point &point)
 {
-    for (const Point &bp : basePoints)
+    basePoints.push_back(point);
+    const Point &p = basePoints.back();
+
+    for (size_t i = 0; i < basePoints.size() - 1; ++i)
     {
+        const Point &bp = basePoints[i];
         addLineToBase(Line(p, bp));
         addCircleToBase(Circle(p, bp));
-        cout << "here0\n";
-        for (const auto cir : baseCircles)
-        {
-            cout << cir << endl;
-        }
         addCircleToBase(Circle(bp, p));
-        cout << "here1\n";
-        for (const auto cir : baseCircles)
-        {
-            cout << cir << endl;
-        }
-    }
-    cout << "here2\n";
-    for (const auto cir : baseCircles)
-    {
-        cout << cir << endl;
-    }
-    if (basePoints.size() == 0)
-        basePoints.push_back(p);
-    cout << "here3\n";
-    for (const auto cir : baseCircles)
-    {
-        cout << cir << endl;
     }
 }
 
-void Builder::addLineToBase(const Line &l)
+void Builder::addLineToBase(const Line &line)
 {
-    if (isNewLine(l))
+    if (isNewLine(line))
     {
-        for (const Line &bline : baseLines)
+        baseLines.push_back(line);
+        const Line &l = baseLines.back();
+
+        for (size_t i = 0; i < baseLines.size() - 1; ++i)
         {
-            const size_t count = intersect(l, bline, res1);
+            const size_t count = intersect(l, baseLines[i], res1);
             if (count == 1 && isNewPoint(res1))
             {
                 newStagePoints.push_back(res1);
@@ -128,7 +158,7 @@ void Builder::addLineToBase(const Line &l)
         for (const Circle &bcircle : baseCircles)
         {
             const size_t count = intersect(l, bcircle, res1, res2);
-            if (count == 1 && isNewPoint(res1))
+            if (count >= 1 && isNewPoint(res1))
             {
                 newStagePoints.push_back(res1);
             }
@@ -137,18 +167,22 @@ void Builder::addLineToBase(const Line &l)
                 newStagePoints.push_back(res2);
             }
         }
-        baseLines.push_back(l);
     }
 }
 
-void Builder::addCircleToBase(const Circle &c)
+void Builder::addCircleToBase(const Circle &circle)
 {
-    if (isNewCircle(c))
+    if (isNewCircle(circle))
     {
+        baseCircles.push_back(circle);
+        const Circle &c = baseCircles.back();
+        // cout << "circle " << c << " basel " << baseLines.size() << " basec " << baseCircles.size() << endl;
+
         for (const Line &bline : baseLines)
         {
             const size_t count = intersect(bline, c, res1, res2);
-            if (count == 1 && isNewPoint(res1))
+            // cout << "inter " << bline << " " << c << " count " << count << endl;
+            if (count >= 1 && isNewPoint(res1))
             {
                 newStagePoints.push_back(res1);
             }
@@ -157,10 +191,11 @@ void Builder::addCircleToBase(const Circle &c)
                 newStagePoints.push_back(res2);
             }
         }
-        for (const Circle &bcircle : baseCircles)
+        for (size_t i = 0; i < baseCircles.size() - 1; ++i)
         {
-            const size_t count = intersect(c, bcircle, res1, res2);
-            if (count == 1 && isNewPoint(res1))
+            const size_t count = intersect(c, baseCircles[i], res1, res2);
+            // cout << "inter " << c << " " << baseCircles[i] << " count " << count << endl;
+            if (count >= 1 && isNewPoint(res1))
             {
                 newStagePoints.push_back(res1);
             }
@@ -169,8 +204,6 @@ void Builder::addCircleToBase(const Circle &c)
                 newStagePoints.push_back(res2);
             }
         }
-        cout << "adding circle " << c << " pars " << *c.par1 << " " << *c.par2 << baseCircles.size() << endl;
-        baseCircles.push_back(c);
     }
 }
 
@@ -180,14 +213,6 @@ void Builder::doStep()
     {
         addPointToBase(sp);
     }
-    for (const auto cir : baseCircles)
-    {
-        cout << cir << endl;
-    }
     stagePoints = newStagePoints;
     newStagePoints.clear();
-    for (const auto cir : baseCircles)
-    {
-        cout << cir << endl;
-    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@ int main()
 {
     Builder builder;
     builder.doStep();
+    cout << "-----------step2\n";
+    builder.doStep();
     cout << "basepoints\n";
     for (const auto p : builder.basePoints)
     {
@@ -25,6 +27,10 @@ int main()
     for (const auto cir : builder.baseCircles)
     {
         cout << cir << endl;
+    }
+    cout << "stagepoints\n";
+    for (const auto p: builder.stagePoints) {
+        cout << p << endl;
     }
 
     return 0;
